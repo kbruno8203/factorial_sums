937bcade57de90f19ff82b89f510892e6d61ba0e
Kyla Bruno <kylabruno42@gmail.com>
2025-07-18 16:36:53 -0500

    Add files via upload

--- factorial_sums.go
+++ factorial_sums.go
@@ -7,9 +7,6 @@ import (
     "strings"
 )
 
-// GetFactorialSums calculates the sum of the digits of the factorial of n.
-// It returns -1 if n is less than or equal to 0.
-// It raises an error if the factorial has more than 100,000 digits.
 func GetFactorialSums(n int) (int, error) {
     if n <= 0 {
         return -1, nil
@@ -20,13 +17,11 @@ func GetFactorialSums(n int) (int, error) {
         fact.Mul(fact, big.NewInt(int64(i)))
     }
 
-    // Check if the factorial has more than 100,000 digits
     factStr := fact.String()
     if len(factStr) > 100000 {
         return 0, errors.New("factorial has more than 100,000 digits")
     }
 
-    // Calculate the sum of the digits
     sum := 0
     for _, digit := range strings.Split(factStr, "") {
         d, err := strconv.Atoi(digit)
